e437c02302c8b14fa5996aa42de67bad3c001627
CarloFanelli <carlo.fanelli.sax@gmail.com>
2023-08-25 15:06:46 +0200

    integrate bootstrap in index

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,20 +5,31 @@ e per i multipli di 5 stampi “Buzz”.
 Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”.
 */
 
-let value;
 
-for (let i = 0 ; i <= 100; i++) {
-   if (i % 3 == 0 && i % 5 != 0) {
+for (let i = 0; i <= 100; i++) {
+    
+    let value;
+
+    if (i % 3 == 0 && i % 5 != 0) {
+
         value = 'Fizz';
-       console.log(value);    
-    }else if (i % 5 == 0 && i % 3 != 0 ) {
+        console.log(value);
+
+    } else if (i % 5 == 0 && i % 3 != 0) {
+
         value = 'Buzz';
         console.log(value);
-    }else if (i % 3 == 0 && i % 5 == 0) {
+
+    } else if (i % 3 == 0 && i % 5 == 0) {
+
         value = 'FizzBuzz';
         console.log(value);
-    }else{
+
+    } else {
+        
         value = i;
         console.log(value);
+
     }
+
 }
